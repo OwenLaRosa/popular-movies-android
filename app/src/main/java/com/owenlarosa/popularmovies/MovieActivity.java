e201be1ffe35ac4ea491b3f6e663dd8ded8f5e92
OwenLaRosa <owen.larosa@outlook.com>
2016-06-29 16:50:19 -0400

    feat: Disable settings menu, settings screen is not needed

--- app/src/main/java/com/owenlarosa/popularmovies/MovieActivity.java
+++ app/src/main/java/com/owenlarosa/popularmovies/MovieActivity.java
@@ -29,9 +29,9 @@ public class MovieActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }

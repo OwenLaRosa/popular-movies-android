417bc27f28d59f05176d443497c7e676b6dfa5e1
OwenLaRosa <owen.larosa@outlook.com>
2016-08-05 11:30:44 -0400

    feat: Add expand/collapse button for review content

--- app/src/main/java/com/owenlarosa/popularmovies/ReviewView.java
+++ app/src/main/java/com/owenlarosa/popularmovies/ReviewView.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -24,6 +26,7 @@ public class ReviewView extends LinearLayout {
 
     @BindView(R.id.review_author_textview) TextView authorTextView;
     @BindView(R.id.review_content_textview) TextView contentTextView;
+    @BindView(R.id.review_expand_button) Button expandReviewButton;
 
     Unbinder unbinder;
 
@@ -47,6 +50,21 @@ public class ReviewView extends LinearLayout {
         View rootView = inflater.inflate(R.layout.review_view, this);
 
         unbinder = ButterKnife.bind(this, rootView);
+
+        expandReviewButton.setText("EXPAND");
+    }
+
+    @OnClick(R.id.review_expand_button)
+    public void toggleExpandedState(View view) {
+        if (expandReviewButton.getText() == "EXPAND") {
+            // max_value will allow the text field to expand infinitely, as needed
+            contentTextView.setMaxLines(Integer.MAX_VALUE);
+            expandReviewButton.setText("COLLAPSE");
+        } else {
+            // 4 is the default maximum number of lines of text
+            contentTextView.setMaxLines(4);
+            expandReviewButton.setText("EXPAND");
+        }
     }
 
 }

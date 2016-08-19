02c5319651476265605bb45339ea6b0c2e3f051e
OwenLaRosa <owen.larosa@outlook.com>
2016-08-19 13:57:55 -0400

    fix: Show/hide popover detail when appropriate.

--- app/src/main/java/com/owenlarosa/popularmovies/MovieActivity.java
+++ app/src/main/java/com/owenlarosa/popularmovies/MovieActivity.java
@@ -5,23 +5,60 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import com.owenlarosa.popularmovies.db.Movie;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 
 public class MovieActivity extends AppCompatActivity implements MovieActivityFragment.Callback {
 
     private boolean mTwoPane;
 
     private static final String DETAILFRAGMENT_TAG = "DFTAG";
+    ImageButton dismissDetailButton;
+    @BindView(R.id.movie_detail_container) FrameLayout movieDetailContainer;
+    View overlay;
+    Unbinder unbinder;
+
+    // whether or not the detail popover should be shown next creation
+    private static final String SHOULD_SHOW_DETAIL_KEY = "shouldShowDetail";
+    boolean shouldShowDetail = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+        unbinder = ButterKnife.bind(this);
 
         getSupportActionBar().setElevation(0);
 
+        if (savedInstanceState != null && savedInstanceState.getBoolean(SHOULD_SHOW_DETAIL_KEY)) {
+            shouldShowDetail = true;
+        }
+
+        overlay = findViewById(R.id.overlay);
+        dismissDetailButton = (ImageButton) findViewById(R.id.dismiss_detail_button);
         if (findViewById(R.id.movie_detail_container) != null) {
+            if (dismissDetailButton != null) {
+                dismissDetailButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        shouldShowDetail = false;
+                        setDetailVisible(false);
+                    }
+                });
+                if (shouldShowDetail) {
+                    setDetailVisible(true);
+                }
+            }
             mTwoPane = true;
         } else {
             mTwoPane = false;
@@ -29,6 +66,11 @@ public class MovieActivity extends AppCompatActivity implements MovieActivityFra
 
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(SHOULD_SHOW_DETAIL_KEY, shouldShowDetail);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,6 +104,12 @@ public class MovieActivity extends AppCompatActivity implements MovieActivityFra
                 return;
             }
 
+            if (movieDetailContainer != null && movieDetailContainer.getVisibility() == GONE) {
+                setDetailVisible(true);
+            }
+            // make the popover visible next time the activity is created
+            shouldShowDetail = true;
+
             Bundle args = new Bundle();
             args.putSerializable(Movie.class.getSimpleName(), movie);
 
@@ -76,4 +124,12 @@ public class MovieActivity extends AppCompatActivity implements MovieActivityFra
             startActivity(intent);
         }
     }
+
+    private void setDetailVisible(boolean flag) {
+        int state = (flag) ? VISIBLE : GONE;
+        movieDetailContainer.setVisibility(state);
+        dismissDetailButton.setVisibility(state);
+        overlay.setVisibility(state);
+    }
+
 }
